102ef9e15ebaca26304a3a395e995a42cb6afcab
GplCart <gplcart.software@gmail.com>
2017-11-06 17:33:53 +0200

    Core API changes

--- Chart.php
+++ Chart.php
@@ -9,7 +9,8 @@
 
 namespace gplcart\modules\chart;
 
-use gplcart\core\Module;
+use gplcart\core\Module,
+    gplcart\core\Config;
 
 /**
  * Main class for Chart module
@@ -18,11 +19,11 @@ class Chart extends Module
 {
 
     /**
-     * Constructor
+     * @param Config $config
      */
-    public function __construct()
+    public function __construct(Config $config)
     {
-        parent::__construct();
+        parent::__construct($config);
     }
 
     /**
