542487d94c56c26018ca6d886c7e60ae1318c867
GplCart <gplcart.software@gmail.com>
2018-02-04 22:08:47 +0200

    Abandoned

--- Main.php
+++ Main.php
@@ -9,19 +9,25 @@
 
 namespace gplcart\modules\chart;
 
+use gplcart\core\Hook;
+
 /**
  * Main class for Chart module
  */
 class Main
 {
+    /**
+     * Hook class instance
+     * @var \gplcart\core\Hook $hook
+     */
+    protected $hook;
 
     /**
-     * Implements hook "construct.controller.backend"
-     * @param \gplcart\core\controllers\backend\Controller $controller
+     * @param Hook $hook
      */
-    public function hookConstructControllerBackend($controller)
+    public function __construct(Hook $hook)
     {
-        $this->setModuleAssets($controller);
+        $this->hook = $hook;
     }
 
     /**
@@ -31,26 +37,88 @@ class Main
     public function getHandlers()
     {
         $handlers = array();
-        foreach (glob(__DIR__ . '/js/handlers/*.js') as $file) {
-            $handlers[] = pathinfo($file, PATHINFO_FILENAME);
-        }
+        $this->hook->attach('module.chart.handlers', $handlers);
 
+        gplcart_array_sort($handlers);
         return $handlers;
     }
 
     /**
-     * Sets module specific assets
-     * @param \gplcart\core\controllers\backend\Controller $controller
+     * Implements hook "construct.controller"
+     * @param \gplcart\core\Controller $controller
      */
-    protected function setModuleAssets($controller)
+    public function hookConstructController($controller)
     {
-        if (!$controller->isInternalRoute()) {
-            $handlers = $this->getHandlers();
-            if (!empty($handlers)) {
-                $controller->setJs('system/modules/chart/js/common.js');
-                $controller->setJsSettings('chart', array('handlers' => $handlers));
+        $this->setJs($controller);
+    }
+
+    /**
+     * Sets chart assets
+     * @param \gplcart\core\Controller $controller
+     * @return null
+     */
+    protected function setJs($controller)
+    {
+        $handlers = $this->getContextHandlers($controller);
+
+        if (empty($handlers)) {
+            return null;
+        }
+
+        $controller->setJs('system/modules/chart/js/common.js', array('aggregate' => false));
+
+        $settings = array();
+        foreach ($handlers as $handler_id => $handler) {
+
+            foreach ((array) $handler['file'] as $file) {
+                $controller->setJs($file, array('aggregate' => false, 'position' => 'top'));
+            }
+
+            if (!empty($handler['settings'])) {
+                $settings[$handler_id] = $handler['settings'];
             }
         }
+
+        if (!empty($settings)) {
+            $controller->setJsSettings('chart', $settings);
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns an array of handlers for the current context
+     * @param \gplcart\core\Controller $controller
+     * @return array
+     */
+    protected function getContextHandlers($controller)
+    {
+        if ($controller->isInternalRoute()) {
+            return array();
+        }
+
+        $handlers = $this->getHandlers();
+
+        foreach ($handlers as $handler_id => $handler) {
+
+            if (empty($handler['file'])) {
+                unset($handlers[$handler_id]);
+                continue;
+            }
+
+            if (!isset($handler['path'])) {
+                continue;
+            }
+
+            foreach ((array) $handler['path'] as $path) {
+                if (!$controller->path($path)) {
+                    unset($handlers[$handler_id]);
+                    break;
+                }
+            }
+        }
+
+        return $handlers;
     }
 
 }
--- js/common.js
+++ js/common.js
@@ -4,34 +4,18 @@
     "use strict";
 
     /**
-     * Draw charts on the page
+     * Load Google Chart library when DOM is ready
      * @returns {undefined}
      */
-    Gplcart.onload.drawCharts = function () {
-        var charts = $('[data-chart-id][data-chart-source]');
-        if (charts.size() && (typeof google === 'undefined' || typeof google.charts === 'undefined')) {
+    Gplcart.onload.loadGoogleChartLibrary = function () {
+        if (typeof google === 'undefined' || typeof google.charts === 'undefined') {
             $.getScript('https://www.gstatic.com/charts/loader.js', function () {
                 if (google !== 'undefined' && google.charts !== 'undefined') {
                     google.charts.load('current', {'packages': ['corechart']});
-                    callHandlers(charts);
+                    Gplcart.hook('module.chart.library.load', google);
                 }
             });
         }
-    };
-
-    /**
-     * Load JS handlers
-     * @param {type} charts
-     * @returns {undefined}
-     */
-    var callHandlers = function (charts) {
-        var id;
-        charts.each(function (i, el) {
-            id = $(el).data('chart-id');
-            if ($.inArray(id, Gplcart.settings.chart.handlers) !== -1) {
-                $.getScript(Gplcart.settings.base + 'system/modules/chart/js/handlers/' + id + '.js', function () {});
-            }
-        });
     };
 
 })(Gplcart, jQuery);
